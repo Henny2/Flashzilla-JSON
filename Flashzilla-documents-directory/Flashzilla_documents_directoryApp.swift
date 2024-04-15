7517d3e4cc3ff962a95799d1f7f5aff5e6f64b29
Henny2 <henrieke.baunack@web.de>
2024-04-14 15:37:45 -1000

    Initial Commit

---
+++ Flashzilla-documents-directory/Flashzilla_documents_directoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flashzilla_documents_directoryApp.swift
+//  Flashzilla-documents-directory
+//
+//  Created by Henrieke Baunack on 4/14/24.
+//
+
+import SwiftUI
+
+@main
+struct Flashzilla_documents_directoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
